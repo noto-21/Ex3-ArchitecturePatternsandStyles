72f45f4e74ba02a393097d10983dd77b82a522ab
NT <128177967+noto-21@users.noreply.github.com>
2023-09-29 22:29:29 -0400

    Big push

--- CashRegister/src/DBReader.java
+++ CashRegister/src/DBReader.java
@@ -1,25 +1,32 @@
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.text.DecimalFormat;
 
 public class DBReader 
 {
+    public static final String path = "CashRegister/src/DB.txt";
+    public static final int attLength = 3;
+
     public static void query(String query)
     {
         DecimalFormat df = new DecimalFormat("0.00");
         String name = "Not Found";
+        int id = 0;
         double price = 0;
         
-        try (Scanner sc = new Scanner(new File("CashRegister/src/DB.txt"));)
+        try (Scanner sc = new Scanner(new File(path));)
         {
             while (sc.hasNext())
             {
                 String read = sc.nextLine();
                 String[] itemArr = read.split(",");
 
-                if (itemArr.length == 3 && itemArr[0].equals(query))
+                if (itemArr.length == attLength && itemArr[0].equals(query))
                 {
+                    id = Integer.parseInt(itemArr[0]);
                     name = itemArr[1];
                     price = Double.parseDouble(itemArr[2]);
                     break;
@@ -37,6 +44,18 @@ public class DBReader
             err.printStackTrace();
         }
 
-        System.out.println(name + ": $" + df.format(price));
+        System.out.println(String.format("%s [%d]: $%s", name, id, df.format(price)));
+    }
+
+    public static void addItem(int id, String name, double price) 
+    {
+        try (FileWriter fw = new FileWriter(path, true))
+        {
+            fw.write("\n" + String.format("%d,%s,%f", id, name, price));
+        }
+        catch (IOException ioe) 
+        {
+            System.err.println("IOException: " + ioe.getMessage());
+        }
     }
 }
--- CashRegister/test/Main.java
+++ CashRegister/test/Main.java
@@ -1,17 +1,10 @@
-/*
- * DB.txt contents:
- * 100825559,Banana,0.47
- * 345712368,Apple,1.12
- * 248736911,Mango,1.20
- * 133874926,HD Television,420.00
- */
-
 public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Test 1:");
         DBReader.query("345712368");
         DBReader.query("133874926");
+        DBReader.query("100825559");
+        DBReader.query("248736911");
     }
 }
